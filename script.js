91001f5ed2de5bb75fb0b0329327ce6a8beaaf41
Freddy Huitron Garcia <78370287+HuitronFreddy@users.noreply.github.com>
2023-11-23 06:13:00 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+  
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+  
+
+document.querySelectorAll('.navbar-nav a').forEach(navItem => {
+  navItem.addEventListener('click', function () {
+  document.querySelector('.navbar-collapse').classList.remove('show');
+    });
+  });
+});
